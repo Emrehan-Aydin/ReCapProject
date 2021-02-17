99b63d573df5ffc73f02c498dc3d7377d65d2c24
EMREHAN AYDIN <emrehan.aydin.tr@gmail.com>
2021-02-17 15:03:09 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    interface ICarDal
+    {
+        void GetByID(Car car);
+        void GetAll(Car car);
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    public class Car:IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public short ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
